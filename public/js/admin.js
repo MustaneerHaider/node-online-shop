5cd9cbba61a38cde3d1dcf91900d86ad55ae48b0
fahadtariq <abdullahtariqfahad@gmail.com>
2022-01-15 12:47:08 +0500

    first-commit

---
+++ public/js/admin.js
@@ -0,0 +1,23 @@
+function deleteProduct(btn) {
+	const prodId = btn.parentNode.querySelector('[name=productId]').value;
+	const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+	const prodEl = btn.closest('article');
+
+	fetch('/admin/product/' + prodId, {
+		method: 'DELETE',
+		headers: {
+			'csrf-token': csrf
+		}
+	})
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			console.log(data);
+			prodEl.parentNode.removeChild(prodEl);
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
